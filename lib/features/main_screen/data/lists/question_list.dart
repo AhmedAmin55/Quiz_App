342dfb5ab8b86afe68c7d0653f9965c52cc8ed18
Ahmed Amin <ahmedamin.pro2@gmail.com>
2024-06-06 11:02:42 +0300

    for another time

---
+++ lib/features/main_screen/data/lists/question_list.dart
@@ -0,0 +1,10 @@
+import 'package:quizapp_clone/features/main_screen/data/models/question_model.dart';
+
+List<QuestionModel> questionsAndAnswers=[
+  QuestionModel(question: "ما هي السُّورة التي تعدُّ أمُّ الكتاب؟", answers:["البقرة","الأخلاص","الفاتحة",], correctAnswer: "الفاتحة", selectedAnswer: null),
+  QuestionModel(question: "متى فرض الصِّيام على المسلمين؟", answers:["السَّنة الثَّانية للهجرة","السَّنة الخامسة للهجرة","السَّنة العاشرة للهجرة","السَّنة الثامنة للهجرة"], correctAnswer: "السَّنة الثَّانية للهجرة", selectedAnswer: null),
+  QuestionModel(question: "ما هي قبلة المسلمين الأولى التي كانوا يصلُّون نحوها؟", answers:["المسجد النبوي ","المسجد ,الحرام","الطائف","المسجد الأقصى"], correctAnswer: "المسجد الأقصى", selectedAnswer: null),
+  QuestionModel(question: "ما اسم السَّجود الذي يسجده المسلم ليجبر نقصاً في صلاته؟", answers:["السَّنة الثَّانية للهجرة ","السَّنة الخامسة للهجرة","سجود السهو","السَّنة الثامنة للهجرة"], correctAnswer: "سجود السهو", selectedAnswer: null),
+  QuestionModel(question: "ممَّ خلقت الملائكة؟", answers:["نور","طين","نار","مياه"], correctAnswer: "نور", selectedAnswer: null),
+  QuestionModel(question: "ما عدد ابواب الجنَّة؟", answers:["20","8","12","3"], correctAnswer: "8", selectedAnswer: null),
+];
--- lib/features/main_screen/view/main_screen.dart
+++ lib/features/main_screen/view/main_screen.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:quizapp_clone/core/shared_widgets/custom_appbar.dart';
+import 'package:quizapp_clone/core/utilis/app_colors.dart';
 import 'package:quizapp_clone/core/utilis/app_texts.dart';
+import 'package:quizapp_clone/features/main_screen/data/lists/question_list.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+class _MainScreenState extends State<MainScreen> {
+  int questionIndex=0;
+  plusQuestionIndex(){
+    if(questionIndex < questionsAndAnswers.length-1)
+    {
+      questionIndex++;
+      setState(() {});
+      print(questionIndex);
+      print(questionsAndAnswers.length);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +33,29 @@ class MainScreen extends StatelessWidget {
         toolbarHeight: size.height*0.11 ,
       ),
       drawer: const Drawer(),
-      body: Column(
-        children: [
-
-        ],
-      ),
+      body:Column(
+          children: [
+            Padding(
+              padding:  EdgeInsets.only(top: size.height*0.120),
+              child: Container(
+                alignment: Alignment.center,
+                child: Text(
+                  questionsAndAnswers[questionIndex].question!,
+                  style:const TextStyle(
+                    color: AppColors.mainColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+              ),
+            ),
+            TextButton(
+                onPressed: (){
+                  plusQuestionIndex();
+                },
+                child:Text("Click"))
+          ],
+        ),
     );
   }
 }
